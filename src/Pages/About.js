a8ac611c4b4315354762d17d7e9137a2734e37cd
Mill Soper <jmsoper@protonmail.com>
2021-11-28 11:50:06 -0800

    Change colors, add CNAME, fill in About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,8 +7,10 @@ const About = () => {
             <div className="content-block">
                 <Link to="/" className="back button">{`< home `}</Link>
                 <h2>About</h2>
-                <p>I make websites.</p>
-                <p>Currently, I'm really interested in shaders.</p>
+                <p>My name is <span className="text-highlight">Mill Soper</span>. I'm a developer currently based in California and working remotely.</p>
+                <p>I work on the full stack with a focus on JavaScript and React.</p>
+                <p>In my free time, I especially like to learn about shaders, SVGs, and accessibility.</p>
+                <p>You can take a look at some of my projects <Link className="link" to="/projects">here</Link>, or check out what I've been up to on <a className="link" href="https://glitch.com/@househaunt">Glitch</a> or <a className="link" href="www.github.com/millsoper">Github</a>.</p>
             </div>
         )
     }
