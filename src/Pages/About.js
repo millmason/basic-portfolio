a5bcff05acf39a367a403798fccd8f814b4c7217
Peter deHaan <pdehaan@hey.com>
2023-06-05 00:31:21 -0700

    Update About.js
    Add https:// to GitHub link to fix 404.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <p>My name is <span className="text-highlight">Mill Soper</span>. I'm a developer currently based in California and working remotely.</p>
                 <p>I work on the full stack with a focus on JavaScript and React.</p>
                 <p>In my free time, I especially like to learn about shaders, SVGs, and accessibility.</p>
-                <p>You can take a look at some of my projects <Link className="link" to="/projects">here</Link>, or check out what I've been up to on <a className="link" href="https://glitch.com/@househaunt">Glitch</a> or <a className="link" href="www.github.com/millsoper">Github</a>.</p>
+                <p>You can take a look at some of my projects <Link className="link" to="/projects">here</Link>, or check out what I've been up to on <a className="link" href="https://glitch.com/@househaunt">Glitch</a> or <a className="link" href="https://www.github.com/millsoper">Github</a>.</p>
             </div>
         )
     }
