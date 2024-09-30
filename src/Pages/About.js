ec318a3cf68430e3cbbda57883ae744d88f79938
Mill Soper <jmsoper@protonmail.com>
2024-09-30 19:25:48 -0400

    copy update, style update

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <div className="content-block">
                 <Link to="/" className="back button">{`< home `}</Link>
                 <h2>About</h2>
-                <p>My name is <span className="text-highlight">Mill Mason</span>. I'm a developer and artist based in Brooklyn. I like to make things that add color and simplicity to life.</p>
+                <p>My name is <span className="text-highlight">Mill Mason</span>. I'm a developer and artist based in Brooklyn. I like to make things that add color and remove complexity.</p>
                 <p>I work on the full stack with a focus on JavaScript and React.</p>
                 <p>In my free time, I especially like to learn about shaders, SVGs, and accessibility.</p>
                 <p>You can take a look at some of my code projects <Link className="link" to="/projects">here</Link>, or check out what I've been up to on <a className="link" href="https://glitch.com/@househaunt">Glitch</a> or <a className="link" href="www.github.com/millsoper">Github</a>.</p>
