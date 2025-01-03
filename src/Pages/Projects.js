b80e01d5e4ca16ec754518106b67c4dc106c3170
millmason <jmsoper@protonmail.com>
2025-01-03 12:53:46 -0500

    Update github links

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,8 +5,8 @@ import './Projects.css';
 
 const Projects = () => {
     const PROJECTS = [
-        { projectName: 'wavy', copy: 'an audio visualizer using the HTML5 audio API', codeUrl: 'https://github.com/millsoper/wavy', liveUrl: 'http://millsoper.github.io/wavy'},
-        { projectName: 'lines', copy: 'animating randomized SVG paths without greensock', codeUrl: 'https://github.com/millsoper/svg-path-animation', liveUrl: 'http://millsoper.github.io/svg-path-animation'},
+        { projectName: 'wavy', copy: 'an audio visualizer using the HTML5 audio API', codeUrl: 'https://github.com/millmason/wavy', liveUrl: 'http://millmason.github.io/wavy'},
+        { projectName: 'lines', copy: 'animating randomized SVG paths without greensock', codeUrl: 'https://github.com/millmason/svg-path-animation', liveUrl: 'http://millmason.github.io/svg-path-animation'},
         { projectName: 'mouse trap', copy: 'experiments with cursor tracking', codeUrl: 'https://glitch.com/edit/#!/mouse-trap', liveUrl: 'https://mouse-trap.glitch.me'},
         { projectName: 'star chart', copy: 'an interactive star chart built with styled components and react hooks', codeUrl: 'https://glitch.com/edit/#!/star-chart', liveUrl: 'https://star-chart.glitch.me'},
         { projectName: 'roman holiday', copy: 'find the next ancient roman holiday via a node and express app', codeUrl: 'https://glitch.com/edit/#!/roman-holiday', liveUrl: 'https://roman-holiday.glitch.me'},
