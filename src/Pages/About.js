704e747ad5f1fa1ffe3eca87a6ecd5422e62adfe
millmason <jmsoper@protonmail.com>
2025-01-10 15:07:03 -0500

    update copy

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
                 <h2>About</h2>
                 <p>My name is <span className="text-highlight">Mill Mason</span>. I'm a developer and artist, and I like to make things that add color and remove complexity.</p>
                 <p>I work on the full stack with a focus on JavaScript and React.</p>
-                <p>In my free time, I especially like to learn about shaders, SVGs, and accessibility.</p>
+                <p>In my free time, I especially like to learn about animation, civic tech, and accessibility.</p>
                 <p>You can take a look at some of my code projects <Link className="link" to="/projects">here</Link>, or check out what I've been up to on <a className="link" href="https://glitch.com/@househaunt">Glitch</a> or <a className="link" href="https://github.com/millsoper">Github</a>.</p>
             </div>
         )
