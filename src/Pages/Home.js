837d980bae5ea2d0709a9494e1fb9f94a76216b8
millmason <jmsoper@protonmail.com>
2025-01-13 13:54:11 -0500

    Add animated gradient, change colors

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,32 @@
-import '../App.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Lines } from '../components/Lines'
+import "../App.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Lines } from "../components/Lines";
 import "./Home.css";
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div className="content-block home">
-                <Lines isOnHomePage={true}/>
-                <div className="title-block">
-                    <div className="text-block">
-                        <p>Hi, I'm Mill</p>
-                    </div>
-                    <div className="button-wrapper">
-                    <Link className="button" to="/projects">projects</Link>
-                    <Link className="button" to="/about">about</Link>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="content-block home">
+        <Lines isOnHomePage={true} />
+        <div className="title-block">
+          <div className="text-block">
+            <p>
+              Hi, I'm <span className="text-highlight">Mill</span>
+            </p>
+          </div>
+          <div className="button-wrapper">
+            <Link className="button" to="/projects">
+              projects
+            </Link>
+            <Link className="button" to="/about">
+              about
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
